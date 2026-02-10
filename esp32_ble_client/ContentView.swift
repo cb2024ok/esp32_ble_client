ee27ea15bf6159b84d8aadd046c0be7f9f23a9f0
Lee <ar88plus@gmail.com>
2026-02-10 23:13:18 +0900

    Initial Commit

---
+++ esp32_ble_client/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  esp32_ble_client
+//
+//  Created by baby Enjhon on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ esp32_ble_client/esp32_ble_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  esp32_ble_clientApp.swift
+//  esp32_ble_client
+//
+//  Created by baby Enjhon on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct esp32_ble_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
